d4d252165d14e901fe6a84edae1631145b49dd07
EL AISSOUSSI Monsif <melaisoussi@sqli.com>
2016-12-19 15:24:27 +0000

    fix searche depth

--- src/ma/elaissoussi/ygraph/YGraph.java
+++ src/ma/elaissoussi/ygraph/YGraph.java
@@ -1,23 +1,20 @@
-package ma.elaissoussi.ygraph ; 
+package ma.elaissoussi.ygraph;
 
 import java.util.Map;
 
-import javax.swing.JFrame;
-
 import ma.elaissoussi.ygraph.core.ExtensionSearcher;
 import ma.elaissoussi.ygraph.graph.DependenciesBuilder;
-import ma.elaissoussi.ygraph.graph.DependenciesViewer;
 
 public class YGraph
 {
-	public static void main(String[] args) 
-	{
-		ExtensionSearcher searcher = new ExtensionSearcher();
-		 String hybrisPath = "D:\\EL AISSOUSSI\\certification\\Hybris\\1-Hybris\\env\\source\\hybris.5.2.0\\hybris";
-		 Map allExtensionDependencies = searcher.getAllExtensionDependencies(hybrisPath);
-		 DependenciesBuilder depbuilder = new DependenciesBuilder(); 
-		 String dependecyGraph = depbuilder.buildDependecyGraph(allExtensionDependencies);
-		 System.out.println(dependecyGraph);
+  public static void main(String[] args)
+  {
+    final ExtensionSearcher searcher = new ExtensionSearcher();
+    final String hybrisPath = "Z:\\source\\hybris";
+    final Map allExtensionDependencies = searcher.getAllExtensionDependencies(hybrisPath);
+    final DependenciesBuilder depbuilder = new DependenciesBuilder();
+    final String dependecyGraph = depbuilder.buildDependecyGraph(allExtensionDependencies);
+    System.out.println(dependecyGraph);
 
-	}
+  }
 }
--- src/ma/elaissoussi/ygraph/core/ExtensionSearcher.java
+++ src/ma/elaissoussi/ygraph/core/ExtensionSearcher.java
@@ -1,64 +1,66 @@
 package ma.elaissoussi.ygraph.core;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ExtensionSearcher 
+public class ExtensionSearcher
 {
-	private static final String BIN = "/bin";
+  private static final String BIN = "/bin/custom";
 
-	private ExtensionReader extensionReader = new ExtensionReader();
+  private final ExtensionReader extensionReader = new ExtensionReader();
 
-	public Map<String, List<String>> getAllExtensionDependencies(String hybrisPath) 
-	{
-	    Map<String, List<String>> extensionDependenies = new HashMap<String, List<String>>(); 
-		
-	    List<String> extensionNames = extensionReader.getExtensionNames(hybrisPath);
-		
-		String hybrisBinDirPath = hybrisPath + File.separator + BIN;
-		
-		File hybrisBinDirecory = new File(hybrisBinDirPath); 
-		
-		for (String extName : extensionNames) 
-		{
-			File extensionDirectory = findExtension(extName, hybrisBinDirecory);
-			List<String> requiredExtensionNames = extensionReader.getRequiredExtensionNames(extensionDirectory);
-			extensionDependenies.put(extName, requiredExtensionNames);
-		}
-		return extensionDependenies;
-	}
+  public Map<String, List<String>> getAllExtensionDependencies(String hybrisPath)
+  {
+    final Map<String, List<String>> extensionDependenies = new HashMap<String, List<String>>();
 
-	private File findExtension(final String extName, File extDirectory) 
-	{
-		if (extName.equals(extDirectory.getName()) && extensionReader.isExtension(extDirectory)) 
-		{
-			return extDirectory;
-		}
-		
-		if(!extensionReader.isExtension(extDirectory) )
-		{
-			File[] extDirectories = extDirectory.listFiles();
-			
-			if (extDirectories != null && extDirectories.length > 0) 
-			{
-				for (File file : extDirectories) 
-				{
-					if (file.isDirectory()) 
-					{
-						File resultFile = findExtension(extName, file);
-	
-						if (resultFile != null) 
-						{
-							return resultFile;
-						}
-					}
-				}
-			}
-		}
-		return null;
-	}
-	
+    final List<String> extensionNames = this.extensionReader.getExtensionNames(hybrisPath);
+
+    final String hybrisBinDirPath = hybrisPath + File.separator + BIN;
+
+    final File hybrisBinDirecory = new File(hybrisBinDirPath);
+
+    for (final String extName : extensionNames)
+    {
+      final File extensionDirectory = findExtension(extName, hybrisBinDirecory);
+      if (extensionDirectory != null)
+      {
+        final List<String> requiredExtensionNames = this.extensionReader.getRequiredExtensionNames(extensionDirectory);
+        extensionDependenies.put(extName, requiredExtensionNames);
+      }
+    }
+    return extensionDependenies;
+  }
+
+  private File findExtension(final String extName, File extDirectory)
+  {
+    if (extName.equals(extDirectory.getName()) && this.extensionReader.isExtension(extDirectory))
+    {
+      return extDirectory;
+    }
+
+    if (!this.extensionReader.isExtension(extDirectory))
+    {
+      final File[] extDirectories = extDirectory.listFiles();
+
+      if (extDirectories != null && extDirectories.length > 0)
+      {
+        for (final File file : extDirectories)
+        {
+          if (file.isDirectory())
+          {
+            final File resultFile = findExtension(extName, file);
+
+            if (resultFile != null)
+            {
+              return resultFile;
+            }
+          }
+        }
+      }
+    }
+    return null;
+  }
+
 }
